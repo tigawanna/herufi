85975716c40436026a963754044215fabc5d334b
tigawanna <denniskinuthiaw@gmail.com>
2023-02-08 19:28:16 +0300

    added shared components

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -10,14 +10,13 @@ export const Categories = ({}:CategoriesProps) => {
 
 return (
  <div className='w-full h-full flex items-center justify-center'>
-        <div className='w-full h-full 
-        flex flex-wrap items-center justify-center gap-2'>
+    <div className='w-full h-full flex flex-col md:flex-row md:flex-wrap items-center justify-center gap-2'>
    {categories.map((item)=>{
     return(
       <Link href={'products/' + item}
         // onClick={()=>navigate('products/'+item)}
-        key={item} className='rounded-xl border  p-2 text-lg
-         shadow-lg hover:shadow-lg hover:scale-95 hover:shadow-slate-300 '>
+        key={item} className='w-full md:w-fit md:rounded-xl border-x md:border  p-2 text-lg
+         shadow-lg hover:shadow-lg  hover:shadow-slate-300 '>
           {item}
         </Link>
     )
